975395cb0c045aed4b18e020d86bcdbb8c554639
Dmitriy Shin <dimashin9@gmail.com>
2020-11-24 22:10:32 -0500

    added users

--- foodapp/foodapp/urls.py
+++ foodapp/foodapp/urls.py
@@ -1,9 +1,9 @@
-"""foodapp URL Configuration"""
-
+from users import views as user_views
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('food/', include('food.urls'))
+    path('food/', include('food.urls')),
+    path('register/', name="register", view=user_views.register)
 ]
---
+++ foodapp/users/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.data.get('username')
+            messages.success(request, f'Welcome {username}, your account is created')
+            return redirect('food:index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/register.html', { 'form': form })
